86fb42c2dca761e3f4cea7bd00da01792111ebbe
Jason Sears <jsears4585@gmail.com>
2017-07-12 15:11:44 -0400

    Added index for community get

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,5 +1,10 @@
 class ArtworksController < ApplicationController
 
+  def index
+    @artworks = Artwork.all
+    render json: @artworks, status: 200
+  end
+
   def create
     @artwork = Artwork.create(title: artwork_params[:title], artwork_HTML: artwork_params[:artwork_HTML], public: artwork_params[:public], user_id: artwork_params[:user_id])
     render json: @artwork, status: 200
