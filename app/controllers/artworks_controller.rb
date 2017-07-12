cdbce841cf37fc2fdf6619029c30c063a6af6231
Jason Sears <jsears4585@gmail.com>
2017-07-12 14:44:15 -0400

    Refactored create artwork post

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,14 +1,13 @@
 class ArtworksController < ApplicationController
 
   def create
-    user = User.find_by_username(artwork_params[:username])
-    Artwork.create(title: artwork_params[:title], artwork_HTML: artwork_params[:artwork_HTML], public: artwork_params[:public], user_id: user.id)
-    byebug
+    @artwork = Artwork.create(title: artwork_params[:title], artwork_HTML: artwork_params[:artwork_HTML], public: artwork_params[:public], user_id: artwork_params[:user_id])
+    render json: @artwork, status: 200
   end
 
   private
     def artwork_params
-      params.permit(:title, :public, :username, :artwork_HTML)
+      params.permit(:title, :public, :user_id, :artwork_HTML)
     end
 
 end
